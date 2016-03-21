0387ea5af821137b8e635d62697d4abda5e76f5c
JessieChen <b09302083@gmail.com>
2016-03-21 22:50:47 +0800

    [1] Finish sample table view and imageView.

--- SampleTable/ViewController.swift
+++ SampleTable/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var restaurantNames = ["7-11", "Family", "OK Market", "Costco", "Apple Store", "Lativ", "H&M", "Zara", "Forever21", "Gap"]
+    
+    var loaderImage = ["cafelore", "barrafina", "bourkestreetbakery", "homei", "posatelier", "teakha", "wafflewolf", "royaloak", "thaicafe", "upstate"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+//        var bundle = NSBundle.mainBundle()
+//        var path = bundle.resourcePath
+        
+        cell.imageView?.image = UIImage(named: loaderImage[indexPath.row], inBundle: NSBundle.mainBundle(),
+            compatibleWithTraitCollection: nil)
+        
+        return cell
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 
 }
 
